2b6f0919694579dcb2ba8fc8e6212d97d2ef1285
nanda2222 <40569077+nanda2222@users.noreply.github.com>
2021-04-01 19:33:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, World 2222!"); 
-        System.out.println("Hello, World Branch!");
-    }
+            }
 }
